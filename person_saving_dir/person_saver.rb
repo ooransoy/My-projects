c9732376de337790c77ebaa0b4deb9962ad1fdab
nolcay <olcay.oransoy@gmail.com>
2013-11-15 19:15:09 +0200

    Json project FINISHED

--- person_saving_dir/person_saver.rb
+++ person_saving_dir/person_saver.rb
@@ -1,5 +1,30 @@
 require 'rubygems'
 require 'json'
 require 'pp'
-class Person
 
+class Person
+  attr_accessor :fname, :mname, :lname, :age
+  def to_hash
+    hash = {}
+    instance_variables.each {|var| hash[var.to_s.delete("@")] = instance_variable_get(var) }
+    hash
+  end
+  def save
+    j = self.to_hash.to_json
+    f = File.open('savefile.json', 'w')
+    f.write(j)
+    f.close
+  end
+end
+
+p = Person.new
+puts "First name:"
+p.fname = gets.chomp.capitalize
+puts "Middle name:"
+p.mname = gets.chomp.capitalize
+puts "Last name:"
+p.lname = gets.chomp.capitalize
+puts "Age:"
+p.age = gets.chomp.to_i
+
+p.save
